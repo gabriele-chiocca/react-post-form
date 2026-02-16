4d7f8a65afa9dc1b1792c06aae077ce0a61c8473
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-16 15:02:24 +0100

    structure form

--- src/App.jsx
+++ src/App.jsx
@@ -23,32 +23,94 @@ export default function App() {
   return (
     <>
       <div className="container">
+        <h1 className="text-center mt-5">Libri</h1>
         <div className="row">
           <div className="col">
-            <div className="mb-3">
-              <label htmlFor="exampleFormControlInput1" className="form-label">
-                Email address
-              </label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleFormControlInput1"
-                placeholder="name@example.com"
-              />
-            </div>
-            <div className="mb-3">
-              <label
-                htmlFor="exampleFormControlTextarea1"
-                className="form-label"
-              >
-                Example textarea
-              </label>
-              <textarea
-                className="form-control"
-                id="exampleFormControlTextarea1"
-                rows="3"
-              ></textarea>
-            </div>
+            <form>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  Author
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Inserisci il nome autore"
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  Title
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Inserisci il titolo del libro"
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Inserisci la tua email"
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlTextarea1"
+                  className="form-label"
+                >
+                  Descrizione
+                </label>
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                  placeholder="Inserisci la descrizione del libro"
+                ></textarea>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="checkDefault"
+                />
+                <label className="form-check-label" htmlFor="checkDefault">
+                  Bozza
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="checkDefaultSecond"
+                />
+                <label className="form-check-label" htmlFor="checkDefault">
+                  Pubblicato
+                </label>
+              </div>
+              <div>
+                <button type="submit" class=" mt-4 btn btn-primary">
+                  Invia
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
