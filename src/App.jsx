d8e639ad58bf8ce1f68eb69ec1e634afbe94c569
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-16 14:29:14 +0100

    structure import axios and initialFormData

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,23 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0);
+const initialFormData = {
+  author: '',
+  title: '',
+  body: '',
+  public: false,
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+export default function App() {
+  axios
+    .get('https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts')
+    .then((res) => {
+      console.log(res.data);
+    });
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  return <></>;
 }
-
-export default App;
