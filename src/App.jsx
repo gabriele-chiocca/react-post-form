35ef3d471106972980d539b0327982d1d9982f46
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-16 14:51:52 +0100

    structure form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import axios from 'axios';
 
+const apiUrl = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
+
 const initialFormData = {
   author: '',
   title: '',
@@ -12,13 +14,44 @@ const initialFormData = {
 };
 
 export default function App() {
-  axios
-    .post('https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts')
-    .then((res) => {
-      console.log(res.data);
-    });
+  axios.post(apiUrl).then((res) => {
+    console.log(res.data);
+  });
 
   const [formData, setFormData] = useState(initialFormData);
 
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                htmlFor="exampleFormControlTextarea1"
+                className="form-label"
+              >
+                Example textarea
+              </label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
